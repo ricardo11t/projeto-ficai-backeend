601f61d2ffd08eb883a6d90827de2b746033a0d7
Gabriel <m3gabriel444@gmail.com>
2025-06-19 19:11:11 -0300

    fazendo alterações

--- routes/admRouter.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const adminController = require('../controllers/adminController.js');
-const { authMiddleware } = require('../middlewares/authMiddleware.js');
-
-router.post('/', authMiddleware, adminController.criarAdmin);
-router.get('/listar-admins', authMiddleware, adminController.listarTodosAdminsEContar);
-router.post('/recuperar-senha', adminController.enviarEmailRecuperacaoSenha);
--- routes/hospedagemRouter.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const hospedagemController = require('../controllers/hospedagemController.js');
-const { authMiddleware } = require('../middlewares/authMiddleware.js');
-
-router.post('/', authMiddleware, hospedagemController.criarHospedagem);
-router.get('/:numero', authMiddleware, hospedagemController.buscarHospedagensporNumero);
